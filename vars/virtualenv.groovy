f87a619e211d41e1fcdafa3a886a1ab8cb13aea7
Gregory Hellings <greg.hellings@gmail.com>
2017-12-10 23:24:01 -0600

    Remove semicolons

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -1,14 +1,14 @@
-#!groovy
+#!/usr/bin/groovy
 
 def call(String name, List packages = []) {
-	def path = "${WORKSPACE}/${name}";
+	def path = "${WORKSPACE}/${name}"
 	// Ensure we hvae a fresh path
 	if( dirExists(path) ) {
-		sh "rm -rf '${path}'";
+		sh "rm -rf '${path}'"
 	}
 
 	sh """virtualenv --no-setuptools --system-site-packages '${name}'
 	      . '${name}/bin/activate'
 	      curl https://bootstrap.pypa.io/get-pip.py | python
-	      pip install ${packages.join(' ')}""";
+	      pip install ${packages.join(' ')}"""
 }
