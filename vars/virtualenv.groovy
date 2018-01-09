47955169a72c0710310275ec5521b22fe87403e9
Gregory Hellings <greg.hellings@gmail.com>
2018-01-09 11:17:25 -0600

    Remove potentially problematic security code

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -1,16 +1,11 @@
 #!/usr/bin/groovy
-import java.nio.file.Files
-import java.nio.file.Paths
-
 def call(String name, List packages = []) {
 	def path = "${WORKSPACE}/${name}"
 	if ( path.contains(" ") ) {
 		throw new Exception("Path cannot include a space")
 	}
 	// Ensure we hvae a fresh path
-	if( Files.exists(Paths.get(path)) ) {
-		sh "rm -rf '${path}'"
-	}
+	sh "rm -rf '${path}'"
 
 	sh """virtualenv --no-setuptools --system-site-packages '${name}'
 	      . '${path}/bin/activate'
